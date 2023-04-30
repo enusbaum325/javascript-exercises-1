992ed7e8a1d4fba011c7a85454a99b5497280e35
enusbaum <enusbaum325@gmail.com>
2023-04-29 22:51:52 -0700

    Updated repeatString function to pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, numTimes) {
+    let outputString = '';
+    if (numTimes < 0) {
+        return 'ERROR';
+    }
+    for (i = 0; i < numTimes; i++) {
+        outputString += inputString;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
