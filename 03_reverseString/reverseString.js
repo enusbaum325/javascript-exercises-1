982e3d5d3aeba10e5cbe331242c3a087850020c3
enusbaum <enusbaum325@gmail.com>
2023-04-29 23:20:38 -0700

    Create reverseString method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    const inputArray = inputString.split('');
+    const outputArray = [];
+    for (i=inputArray.length - 1; i>=0; i--) {
+        outputArray.push(inputArray[i]);
+    }
+    return outputArray.join('');
 };
 
 // Do not edit below this line
