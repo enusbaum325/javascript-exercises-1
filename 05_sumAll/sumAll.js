e92813b101e003b0500a4209ca233fa1b9aaf2a6
enusbaum <enusbaum325@gmail.com>
2023-04-29 23:58:31 -0700

    Implement sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let result = 0;
+    if (Math.min(start,end) < 0) {
+        return 'ERROR';
+    }
+    else if (typeof(start)!= 'number' || typeof(end)!= 'number') {
+        return 'ERROR';
+    }
+    for (i = Math.min(start,end); i <= Math.max(start,end); i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
